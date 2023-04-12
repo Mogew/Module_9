bf483d7bf51a315235f9d47b8eada1da4b95ce56
Mogew <alimovren@gmail.com>
2023-04-12 13:56:19 +0600

    Now it shows all questions in a raw, than disable buttons after last question

--- Module_9/ViewController.swift
+++ Module_9/ViewController.swift
@@ -38,12 +38,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var questionField: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var trueButton: UIButton!
+    let quiz = [
+        "Four + Two is equal Six",
+        "Five - Three is greater than One",
+        "Three + Five is less than Eleven"
+    ]
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        updateUI()
+        
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        guard questionNumber < quiz.endIndex - 1
+        else {
+            endOfQuiz()
+            return
+        }
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI () {
+        questionField.text = quiz[questionNumber]
+    }
+    func endOfQuiz () {
+        print("!")
+        trueButton.isEnabled = false
+        falseButton.isEnabled = false
+    }
 }
 
